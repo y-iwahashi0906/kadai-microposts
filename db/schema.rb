0e58ec39a954838aa4f52c535de8388c040e08f0
youhei.iwahashi <y.iwahashi0906@gmail.com>
2022-09-23 01:47:34 +0000

    user login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_19_122115) do
+ActiveRecord::Schema.define(version: 2022_09_23_013118) do
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2022_09_19_122115) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.date "birthday"
   end
 
 end
